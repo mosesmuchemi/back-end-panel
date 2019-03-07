62464486f9a954af4f46c5bc66b217e52532f262
unknown <mosesmuchemi@yahoo.com>
2019-03-07 13:35:01 +0300

    local host url change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/backend-panel-crud/';
+$config['base_url'] = 'http://localhost/back-end-panel/';
 
 
 
